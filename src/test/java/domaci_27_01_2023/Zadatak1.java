ceee57e98d3f181821214f5d5fb3dcceddb80510
Andrija Antic <andrijant0304@gmail.com>
2023-01-30 09:50:03 +0100

    Domaci 27.01.2023. Zadatak1 dopuna

--- src/test/java/domaci_27_01_2023/Zadatak1.java
+++ src/test/java/domaci_27_01_2023/Zadatak1.java
@@ -2,9 +2,11 @@ package domaci_27_01_2023;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Zadatak1 {
     public static void main(String[] args) throws InterruptedException {
@@ -16,30 +18,36 @@ public class Zadatak1 {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://mdbootstrap.com/docs/standard/components/toasts/#section-basic-example");
 
-        driver.findElement(By.id("basic-primary-trigger")).click();
-        Thread.sleep(1000);
+//        driver.findElement(By.id("basic-primary-trigger")).click();
+//        Thread.sleep(1000);
+//
+//        driver.findElement(By.id("basic-secondary-trigger")).click();
+//        Thread.sleep(1000);
+//
+//        driver.findElement(By.id("basic-success-trigger")).click();
+//        Thread.sleep(1000);
+//
+//        driver.findElement(By.id("basic-danger-trigger")).click();
+//        Thread.sleep(1000);
+//
+//        driver.findElement(By.id("basic-warning-trigger")).click();
+//        Thread.sleep(1000);
+//
+//        driver.findElement(By.id("basic-info-trigger")).click();
+//        Thread.sleep(1000);
+//
+//        driver.findElement(By.id("basic-light-trigger")).click();
+//        Thread.sleep(1000);
+//
+//        driver.findElement(By.id("basic-dark-trigger")).click();
+//        Thread.sleep(1000);
 
-        driver.findElement(By.id("basic-secondary-trigger")).click();
-        Thread.sleep(1000);
+        List<WebElement> buttons = driver.findElements(By.xpath("//*[@id='section-basic-example']/section[1]//button"));
 
-        driver.findElement(By.id("basic-success-trigger")).click();
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("basic-danger-trigger")).click();
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("basic-warning-trigger")).click();
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("basic-info-trigger")).click();
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("basic-light-trigger")).click();
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("basic-dark-trigger")).click();
-        Thread.sleep(1000);
-
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).click();
+            Thread.sleep(1000);
+        }
 
         Thread.sleep(3000);
         driver.quit();
