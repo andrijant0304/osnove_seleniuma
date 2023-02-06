d77f61cfd6c26050a1e4a763cb5aac3e3d6ba60d
Andrija Antic <andrijant0304@gmail.com>
2023-02-06 13:40:14 +0100

    Klasa Helper - dodate metode:downloadUsingStream, takeScreenshot, getHTTPResponseStatusCode

--- src/test/java/helper/Helper.java
+++ src/test/java/helper/Helper.java
@@ -1,9 +1,15 @@
 package helper;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 public class Helper {
 
@@ -18,7 +24,42 @@ public class Helper {
     }
 
     public boolean elementExistByList(WebDriver driver, By by) {
-        return driver.findElements(By.id("id-0")).size() > 0;
+        return driver.findElements(by).size() > 0;
     }
+
+    public void downloadUsingStream(String urlStr, String relativePath) throws IOException, IOException {
+        String file = new File(relativePath).getAbsolutePath();
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(file);
+        byte[] buffer = new byte[1024];
+        int count=0;
+        while((count = bis.read(buffer,0,1024)) != -1)
+        {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
+    }
+
+
+
+    public void takeScreenshot(WebDriver driver, String relativePath) throws IOException {
+        TakesScreenshot ts=(TakesScreenshot)driver;
+        FileHandler.copy(ts.getScreenshotAs(OutputType.FILE),
+                new File(relativePath));
+    }
+
+    public int getHTTPResponseStatusCode(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        return http.getResponseCode();
+    }
+
+
+
+//    public String getFilePath(String relativePath) {
+//        return new File(relativePath).getAbsolutePath();
+//    }
 
 }
